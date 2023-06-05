41496901464fa15283fc6148aec17393d71d14e6
ytakasugi <sh7.tibi0129@gmail.com>
2023-06-05 23:29:16 +0900

    add files

---
+++ examples/ch1-03-spawn-closure.rs
@@ -0,0 +1,23 @@
+use std::thread;
+
+fn main() {
+    let numbers = vec![1, 2, 3];
+
+    thread::spawn(move || { 
+        for n in &numbers {
+            println!("{n}");
+        }
+    }).join().unwrap();
+
+    let arr = Vec::from_iter(0..=1000);
+
+    let t = thread::spawn(move || {
+        let len = arr.len();
+        let sum = arr.iter().sum::<usize>();
+
+        sum / len
+    });
+
+    let avg = t.join().unwrap();
+    println!("Average: {avg}");
+}
