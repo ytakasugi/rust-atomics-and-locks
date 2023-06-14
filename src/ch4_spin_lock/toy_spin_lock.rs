60b0bfb0cc5fb3c6d2667a6b9b135b59d1486f0a
ytakasugi <sh7.tibi0129@gmail.com>
2023-06-14 22:14:51 +0900

    change file

--- src/ch4_spin_lock/toy_spin_lock.rs
+++ src/ch4_spin_lock/toy_spin_lock.rs
@@ -1,3 +1,4 @@
+use std::ops::{Deref, DerefMut};
 use std::cell::UnsafeCell;
 use std::sync::atomic::AtomicBool;
 use std::sync::atomic::Ordering::{Acquire, Release};
@@ -11,6 +12,10 @@ pub struct SpinLock<T> {
 // 安全でない`Sync`(複数スレッドからのアクセスを許可)を実装する
 unsafe impl<T> Sync for SpinLock<T> where T: Send {}
 
+pub struct Guard<'a, T> {
+    lock: &'a SpinLock<T>,
+}
+
 impl<T> SpinLock<T> {
     pub const fn new(value: T) -> Self {
         Self {
@@ -19,18 +24,47 @@ impl<T> SpinLock<T> {
         }
     }
 
-    pub fn lock(&self) -> &mut T {
+    pub fn lock(&self) -> Guard<T> {
         while self.locked.swap(true, Acquire) {
+            // プロセッサにスピンループを通知する
             std::hint::spin_loop();
         }
-        unsafe { &mut *self.value.get() }
+        Guard { lock: self }
     }
-    /// Safety
-    pub unsafe fn unlock(&self) {
-        self.locked.store(false, Release);
+}
+
+impl<T> Deref for Guard<'_, T> {
+    type Target = T;
+    fn deref(&self) -> &T {
+        unsafe { &*self.lock.value.get() }        
+    }
+}
+
+impl<T> DerefMut for Guard<'_, T> {
+    fn deref_mut(&mut self) -> &mut T {
+        unsafe { &mut *self.lock.value.get() }
+    }
+}
+
+impl<T> Drop for Guard<'_, T> {
+    fn drop(&mut self) {
+        self.lock.locked.store(false, Release);
     }
 }
 
 #[test]
 fn main() {
+    use std::thread;
+
+    let x = SpinLock::new(Vec::new());
+    thread::scope(|s|{
+        s.spawn(|| x.lock().push(1));
+        s.spawn(|| {
+            let mut g = x.lock();
+            g.push(2);
+            g.push(2);
+        });
+    });
+    let g = x.lock();
+    assert!(g.as_slice() == [1, 2, 2] || g.as_slice() == [2, 2, 1]);
 }
